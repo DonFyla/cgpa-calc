fb3ebc54e8a8548553c847e7ad1e6e3f912dee74
DonFyla <alabioyetope@gmail.com>
2023-12-19 01:08:28 +0100

    A cumulative gradepoint calculator still in it's skeletal state

---
+++ cgpa_cal.py
@@ -0,0 +1,22 @@
+# A cgpa calclator
+# Parameters required = Number of Total units, grade and unit of each course. 
+# False code """ request for sum of total units. calculate points gotten in each course, divide by total unit.
+
+
+U = int(input("input the total number of units offered: "))
+grade_value = {"A":5,"B":4,'C':3,"D":2,"E":1,"F":0}
+
+sum = 0
+while True:
+   user_input = input("input grade and course unit like so eg. B,3 :")
+   if user_input == "stop":
+      print("your final cgpa is ", current_cgpa)
+      break
+   else:
+      grade, unit = user_input.split(",")
+      grade_point = grade_value[grade]
+      point = int(grade_point)*int(unit)
+      sum+=point
+      current_cgpa =sum/U
+      print ("your current cgpa is ",current_cgpa )
+
